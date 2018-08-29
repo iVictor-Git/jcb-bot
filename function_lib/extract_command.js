24f2d982a1cf1b44cb301097474259a95db881f4
Victor <victor.n.tran@outlook.com>
2018-08-29 15:46:58 -0400

    fix logic error, joined string array

--- function_lib/extract_command.js
+++ function_lib/extract_command.js
@@ -1,5 +1,5 @@
 const extractCommand = (message = '') => {
-    return message.split('').slice(1, message.length);
+    return message.split('').slice(1, message.length).join('');
 }
 
 module.exports = {

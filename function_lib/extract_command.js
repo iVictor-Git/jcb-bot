2fe08bc5702e9f100c22f17c96b2bbc6d69f41c8
Victor <victor.n.tran@outlook.com>
2018-08-29 18:54:03 -0400

    Add comment explaining requirements

--- function_lib/extract_command.js
+++ function_lib/extract_command.js
@@ -1,4 +1,5 @@
 const extractCommand = (message = '') => {
+    // this should only be sent the first argument when splitting the message the user sends, but written to cover more than one word
     if (typeof message !== 'string') {
         return () => {
             throw Error('TypeError');

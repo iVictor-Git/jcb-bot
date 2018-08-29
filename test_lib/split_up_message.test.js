5f8bc9ab632b46f44792197b4ed0ac847935259f
Victor <victor.n.tran@outlook.com>
2018-08-29 19:25:39 -0400

    take the split up message function from the imported file

--- test_lib/split_up_message.test.js
+++ test_lib/split_up_message.test.js
@@ -1,4 +1,4 @@
-const splitUpMessage = require('../function_lib/split_up_message');
+const splitUpMessage = require('../function_lib/split_up_message').splitUpMessage;
 
 test('Given a string message, split the message up', () => {
     expect(splitUpMessage('$command arg1 arg2 arg3 ...')).toContain(['$command', 'arg1', 'arg2', 'arg3', '...'])

636616382d03b3fecf3e355165d0aa28fc5e73b6
Victor <victor.n.tran@outlook.com>
2018-08-29 19:49:07 -0400

    modified code to return a list consisting of the identifier, command, and the args

--- function_lib/split_up_message.js
+++ function_lib/split_up_message.js
@@ -1,3 +1,6 @@
+const extractIdentifier = require('../function_lib/extract_identifier').extractIdentifier,
+    extractCommand = require('../function_lib/extract_command').extractCommand
+
 const splitUpMessage = (message = '') => {
     // if message is not a string, throw an error
     if (typeof message !== 'string') {
@@ -5,8 +8,12 @@ const splitUpMessage = (message = '') => {
             throw Error('Error');
         }
     }
+    const args = message.split(' '),
+        command = extractCommand(args[0]),
+        identifier = extractIdentifier(args[0])
 
-    return message.split(' ');
+
+    return [identifier, command, ...args.slice(1, args.length)];
 
 }
 

de6d007fef31714274a0f9be8bc59ff6046fb98f
Victor <victor.n.tran@outlook.com>
2018-08-29 19:29:02 -0400

    modified test matcher from 'toContain' to 'toEqual'

--- test_lib/split_up_message.test.js
+++ test_lib/split_up_message.test.js
@@ -1,5 +1,5 @@
 const splitUpMessage = require('../function_lib/split_up_message').splitUpMessage;
 
 test('Given a string message, split the message up', () => {
-    expect(splitUpMessage('$command arg1 arg2 arg3 ...')).toContain(['$command', 'arg1', 'arg2', 'arg3', '...'])
+    expect(splitUpMessage('$command arg1 arg2 arg3 ...')).toEqual(['$command', 'arg1', 'arg2', 'arg3', '...'])
 })

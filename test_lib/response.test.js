faa805f4bd1aae9c3fc4722f3551b0b487e96238
Victor <victor.n.tran@outlook.com>
2018-08-29 23:37:49 -0400

    change expected result format

--- test_lib/response.test.js
+++ test_lib/response.test.js
@@ -1,8 +1,18 @@
 const response = require('../function_lib/response').response
 
 test('given the command hi or hello', () => {
-    expect(response('hi')).toBe('hello')
-    expect(response('hello')).toBe('hi')
+    expect(response('hi')).toEqual({
+        err: false,
+        message: 'hello'
+    })
+    expect(response('hello')).toEqual({
+        err: false,
+        message: 'hi'
+    })
+    expect(response('bihh')).toEqual({
+        err: true,
+        message: 'Invalid command, type $help for help'
+    })
 })
 
 test('given non-string, reject', () => {

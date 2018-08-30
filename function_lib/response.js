3eb8ee7d993db0c99ad8f1d97c219219aa46bbe1
Victor <victor.n.tran@outlook.com>
2018-08-29 23:38:17 -0400

    change return value to be an object containing an error boolean and a message

--- function_lib/response.js
+++ function_lib/response.js
@@ -6,7 +6,13 @@ const commands = {
 const response = (message = '') => {
     return typeof message !== 'string' ? () => {
         throw Error('Invalid type given, expected string')
-    } : commands[message]
+    } : commands[message] ? {
+        err: false,
+        message: commands[message]
+    } : {
+        err: true,
+        message: 'Invalid command, type $help for help'
+    }
 }
 
 module.exports = {
